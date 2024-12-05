8cdb96f29f34ace1925bcc5934d8134611394fc4
Laureano Gallegos <laureanogallegos@cetcom.com.ar>
2024-12-05 15:47:12 -0300

    Reporte

--- Controladora/ControladoraReportes.cs
+++ Controladora/ControladoraReportes.cs
@@ -16,11 +16,22 @@ namespace Controladora
 
         public ReadOnlyCollection<Factura> LeerFacturas()
         {
-            return _context.Facturas.Include(c=>c.Cliente).Include(d=>d.DetallesDeFactura).OrderBy(f=>f.Fecha).ToList().AsReadOnly();
+            return _context.Facturas.Include(c => c.Cliente).Include(d => d.DetallesDeFactura).OrderBy(f => f.Fecha).ToList().AsReadOnly();
         }
-        public ReadOnlyCollection<Producto> LeerProductosMasVendidos()
+        public ReadOnlyCollection<ProductoReporte> LeerProductosMasVendidos()
         {
-            return _context.Productos.Include(x=>x.DetalleFactura).ToList().AsReadOnly();//.Where(x=>x.DetallesDeFactura).Include(x=>x.DetallesDeFactura).ThenInclude(p => p.Producto).Sum().OrderByDescending(c => c.Cantidad).ToList().AsReadOnly();
+            var lista = _context.Productos.Include(x => x.DetalleFactura).AsNoTracking();
+            var result = lista.GroupBy(l => l.DetalleFactura.Producto)
+                              .Select(cl => new ProductoReporte
+                              {
+                                  Id = cl.Key.Id,
+                                  Nombre = cl.Key.Nombre,
+                                  Codigo = cl.Key.Codigo,
+                                  Descripcion = cl.Key.Descripcion,
+                                  CantidadVendida = cl.Sum(c => c.DetalleFactura.Cantidad)
+                                  
+                              }).OrderByDescending(p => p.CantidadVendida).ToList().AsReadOnly();
+            return result;
         }
     }
 }
